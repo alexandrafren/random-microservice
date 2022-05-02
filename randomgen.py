295a5122c99ea0bef20101e6cf06c965713542f7
Alexandra Fren <35177685+alexandrafren@users.noreply.github.com>
2022-05-02 18:29:31 -0400

    add in flask app

--- randomgen.py
+++ randomgen.py
@@ -1,30 +1,49 @@
+from ast import Try
 import random
 import string
+import json
+from flask import Flask, jsonify, request
 
-def randomgen(upper = True, lower = True, nums = True, sym = True):
+app = Flask(__name__)
+
+@app.route('/')
+def index():
     return_arr = []
     alpha_arr = []
     symbol_options = ["@", "$", "!", "&", "%"]
     cases = [string.ascii_lowercase, string.ascii_uppercase]
     output_string = ""
+    # parameters
+    upper = "True"
+    lower = "True"
+    nums = "True"
+    sym = "True"
+    if request.args:
+        if request.args.get('upper'):
+            upper = request.args.get('upper')
+        if request.args.get('lower'): 
+            lower = request.args.get('lower')
+        if request.args.get('nums'):
+            nums = request.args.get('nums')
+        if request.args.get('sym'): 
+            sym = request.args.get('sym')
     # generate letters
-    if (upper or lower):
+    if (upper == "True" or lower == "True" or upper == "true" or lower == "true"):
         alpha_arr = random.choices(cases[0], k=5)
         for i in alpha_arr:
             return_arr.append(i)
     # generate numbers
-    if nums:
+    if nums == "True" or nums == "true":
+        print("we should be adding numbers")
         for i in range(0,5):
             n = random.randint(0,9)
             return_arr.append(str(n))
     # generate symbols
-    if sym: 
+    if sym == "True" or sym == "true": 
         for i in range(0,3):
             n = random.randint(0,4)
             return_arr.append(symbol_options[n])
     random.shuffle(return_arr)
     output_string = "".join(return_arr)
-    print(output_string)
-    
-
-randomgen(True, False, False, True)
+    return jsonify({'ran': output_string})
+app.run()
