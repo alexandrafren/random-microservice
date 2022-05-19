2fe65532d0a7133873e32a288ac2c9ac5578fe63
Alexandra Fren <35177685+alexandrafren@users.noreply.github.com>
2022-05-19 15:30:37 -0400

    break index function up

--- randomgen.py
+++ randomgen.py
@@ -7,13 +7,32 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+
+# Method to Generate Random Numbers, takes in a Length, and the array to pass numbers back to
+def gen_nums(ln, return_arr):
+    for i in range(0,ln):
+        n = random.randint(0,9)
+        return_arr.append(str(n))
+
+# Method to Generate Random Symbols, takes in a Length, and the array to pass symbols back to
+def gen_sym(vol, return_arr):
+    symbol_options = ["@", "$", "!", "&", "%"]
+    for i in range(0,vol):
+        n = random.randint(0,4)
+        return_arr.append(symbol_options[n])
+
+# Method to Generate Random Letters, takes in a Length, the case of the letters, and the array to pass letters back to
+def gen_letters(case, ln, return_arr):
+    alpha_arr = random.choices(case, k=ln)
+    for i in alpha_arr:
+        return_arr.append(i)
+
+
 @app.route('/')
 def index():
     vol = 16
     div_count = 0
     return_arr = []
-    alpha_arr = []
-    symbol_options = ["@", "$", "!", "&", "%"]
     output_string = ""
     # parameters
     upper = "True"
@@ -43,32 +62,23 @@ def index():
     if lower == "True" or lower == "true":
         ln = trunc(vol / div_count)
         vol = vol - ln
-        alpha_arr = random.choices(string.ascii_lowercase, k=ln)
-        for i in alpha_arr:
-            return_arr.append(i)
+        gen_letters(string.ascii_lowercase, ln, return_arr)
         div_count -= 1
     # generate uppercase letters
     if upper == "True" or upper == "true":
         ln = trunc(vol / div_count)
         vol = vol - ln
-        alpha_arr = random.choices(string.ascii_uppercase, k=ln)
-        for i in alpha_arr:
-            return_arr.append(i)
+        gen_letters(string.ascii_uppercase, ln, return_arr)
         div_count -= 1
     # generate numbers
-    print(div_count)
     if nums == "True" or nums == "true":
         ln = trunc(vol / div_count)
         vol = vol - ln
-        for i in range(0,ln):
-            n = random.randint(0,9)
-            return_arr.append(str(n))
+        gen_nums(ln, return_arr)
         div_count -= 1
     # generate symbols
     if sym == "True" or sym == "true": 
-        for i in range(0,vol):
-            n = random.randint(0,4)
-            return_arr.append(symbol_options[n])
+        gen_sym(vol, return_arr)
     random.shuffle(return_arr)
     output_string = "".join(return_arr)
     return jsonify({'data': output_string})
